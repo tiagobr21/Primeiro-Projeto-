dc0ba4a72bb36bc5b76858a7e36ba9d5d418f748
tiagobr21 <tiagobrr17@gmail.com>
2022-04-07 22:11:59 -0300

    Put

--- js/post_escala-pessoa.js
+++ js/post_escala-pessoa.js
@@ -1,13 +1,24 @@
  /* API */
 
 
- function fazPost(url,body){
-    console.log("Body=",body)
-    let request = new XMLHttpRequest()
-    request.open("POST",url,true)
-    request.setRequestHeader("Content-Type", "application/json")
-    request.send(JSON.stringify(body))
+
+ function fazPut(){
+  let request = new XMLHttpRequest()
+  request.open('PUT',"https://app-escala-api.herokuapp.com/v1/escalas/94/pessoa/94",false)
+  request.send()
+  return request.responseText
   
+}
+
+
+
+/* 
+
+ function fazPost(url){
+    let request = new XMLHttpRequest()
+    request.open("PUT",url,true)
+    request.setRequestHeader("Content-Type", "application/json")
+
   
   }
   
@@ -15,13 +26,11 @@
   function mandarPessoa(){
    event.preventDefault()
    url = "https://app-escala-api.herokuapp.com/v1/escalas/94/pessoa/94"
-    nome_pessoa = "nome_pessoa"
-   console.log(nome)
-   body ={
-    "nome_pessoa" : nome_pessoa
-   }
   
-   fazPost(url,body)
+  
+ 
+  
+   fazPost(url)
    
   }
-  
+   */

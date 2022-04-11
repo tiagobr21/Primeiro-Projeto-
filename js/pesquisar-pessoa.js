b7081e601ac2c46eedd426153dfdfa30bcea60c8
tiagobr21 <tiagobrr17@gmail.com>
2022-04-11 16:57:27 -0400

    função pesquisa

--- js/pesquisar-pessoa.js
+++ js/pesquisar-pessoa.js
@@ -1,30 +1,32 @@
 // API endpoint --------------------------------------------
 const baseUrl = 'https://app-escala-api.herokuapp.com/v1/pessoas';
-//https://app-escala-api.herokuapp.com/v1/pessoas/findByName/sergio vinhote
-// Get Elements --------------------------------------------
-const searchInput = getElement('.search-input'),
-      searchButton = getElement('.search-button'),
-      main = getElement('.main')
 
-var nomePessoa, // Nome ou numero passado na caixa de busca
-    pessoa, // Responsavel por guardar os dados recebidos da API
-    html; // Responsavel por receber o HTML 
-
-// Build Functions --------------------------------------------
-
-// Função para reduzir a escrita na captura de elementos HTML
-function getElement(element) {
+const searchInput = getElement('#search-input'),
+ searchButton = getElement('#search-button')
+   
+ function getElement(element) {
   return document.querySelector(element);
 }
-
 // Função responsavel por fazer requisições para a API e inserir as respostas na variavel pokemon
-function requestPokeInfo(url, name) {
-  fetch(url + name)
+function pessoaRequest() {
+  fetch(baseUrl)
     .then(response => response.json())
     .then(data => {
-      pessoa = data;
+     console.log(data)
     })
     .catch(err => console.log(err));
 }
 
-console.log(pessoa)
+function startApp(data) {
+  pessoaRequest(baseUrl, data);
+
+  /* setTimeout(function () {
+      data.innerHTML = consultarEscala();
+  }, 2000); */
+}
+
+searchButton.addEventListener('click', event => {
+  event.preventDefault();
+  data = searchInput.value.toLowerCase();
+  startApp(data);
+});
